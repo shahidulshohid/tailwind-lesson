d538fb3b38715db3965d2a1eae7bcb5c9d4457a4
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-03 17:58:52 +0600

    this is first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+      <h1>
+      Hello world!
+    </h1>
+      </div>
+      <div>
+        <p className="text-red-500 text-3xl text-left">Tom</p>
+        <p className="text-gray-400 text-lg text-center">Jerry</p>
+        <p className="text-blue-900 text-xs text-right">Max</p>
+      </div>
+    </>
+  )
+}
+
+export default App
+
