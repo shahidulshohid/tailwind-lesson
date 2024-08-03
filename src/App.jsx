118fd6e812c16a94d129eef951643264161f4c2c
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-03 20:52:10 +0600

    flex practice is done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <div className="text-3xl font-bold underline">
+      {/* <div className="text-3xl font-bold underline">
       <h1>
       Hello world!
     </h1>
@@ -13,7 +13,7 @@ function App() {
         <p className="text-gray-400 text-lg text-center">Jerry</p>
         <p className="text-blue-900 text-xs text-right">Max</p>
       </div>
-      <div className="bg-red-500 min-w-40 min-h-60">
+      <div className="bg-red-500 min-w-4000 min-h-60">
         Tailwind is great
       </div>
       <div className="bg-blue-200 w-1/2 h-32 text-center">
@@ -22,10 +22,18 @@ function App() {
       <div className="bg-gray-900 text-white m-4 p-8">
         Tailwind is Awesome
       </div>
-      {/* <div className="bg-[url('./images/banner.png')] h-screen bg-no-repeat bg-cover bg-center">
-      </div> */}
+      <div className="bg-[url('./images/banner.png')] h-screen bg-no-repeat bg-cover bg-center">
+      </div>
       <div className="bg-gradient-to-r from-blue-300 via-purple-500">
         We are learning gradient
+      </div> */}
+      <div className="flex">
+        <div className="flex-1 border border-green-800">First div 1</div>
+        <div className="flex-2 border border-pink-700">Second div 2</div>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <div>First</div>
+        <div>Second</div>
       </div>
     </>
   )
